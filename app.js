0be5c1f840eba22e21a8d8538f29152b71cab432
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-10-20 16:10:26 +0000

    Minor Issues

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-// Where all the work will be done...
-
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, responce){
-    responce.send('Hello World'); // this is an express function or Express API
-    // You can also use res.write('Hello World'), and then end it with res.end() which is the same as the current Express Function used above
-})
+
+/*app.get('/', function(request, responce) {
+    responce.sendFile(__dirname + '/public/index.html');
+});
+*/
+
+app.use(express.static('public'));
 
 app.get('/blocks', function(request, responce){
- responce.redirect(301, '/parts'); // using the 301 status code it makes it into a permantly redirect
-
-/*var blocks = ['Fixed', 'Movable', 'Rotating'];
-responce.send(blocks); // convert this function into a JSON
-// responce.json(blocks); responds the same as the Send function for object and arrays
-*/
+ var blocks = ['Fixed', 'Movable', 'Rotating'];
+ responce.json(blocks); // creating a array of blocks and synorizing it back to the client 
 })
 
-
-
-/* app.listen(3000, function(){
-    console.log('Listening on port 3000')
-}); // takes an optional call back function as a second arguement, once opening the browser it show the url but also show the port# which is 3000
-
-// changes to code requires server restart
-*/
+app.listen(3000);
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Notes 
 
+
+// Express?
 /*
 
 What Express is..
@@ -12,5 +14,15 @@ Ex... MySpace, Ghost and more
 - Foundation for other tools and framework, like Kraken and Sails
 - Can install Express using npm
 - And to install the latest version of Express you must use the @ sign indicating what version you would like to update it too
+
+*/
+
+// What is MiddleWare
+/*
+
+Middle ware is functions executed sequentially that access request and responce
+There are 3 sections it passes before reaching the route, the 3 are Validation, Authentication, and Data Parsing
+
+The first middle ware 'middleware A' is a function that takes the request and responce of the function as an arguement and then uses the next function to move from MiddleWare A to MiddleWare B, Like a pattern
 
 */
